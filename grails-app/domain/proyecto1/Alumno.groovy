0439c9a6534f96cd3f676c22dc79b50ff277dd73
oscahern <oscahern@mbp-de-daniel.lan>
2014-04-02 21:10:10 -0600

    se añaden validaciones básicas a los atributos

--- grails-app/domain/proyecto1/Alumno.groovy
+++ grails-app/domain/proyecto1/Alumno.groovy
@@ -3,9 +3,16 @@ package proyecto1
 class Alumno {
     
     String nombre;
+    String apPaterno;
+    String apMaterno;
     String correo;
     Date fecha_nac;
     
     static constraints = {
+        nombre(blank:false)
+        apPaterno(blank:false)
+        apMaterno(blank:false)
+        fecha_nac(blank:false)
+        correo(email:true)
     }
 }
