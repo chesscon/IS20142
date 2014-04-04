c842b46712d75ed0a18a3332ad583f6613307a59
Oscar Hdz C <chesscon@gmail.com>
2014-04-03 21:30:46 -0600

    Se añade metodo toString

--- grails-app/domain/proyecto1/Usuario.groovy
+++ grails-app/domain/proyecto1/Usuario.groovy
@@ -4,9 +4,16 @@ class Usuario {
     
     String usuario;
     String passwd;
+    
+    
+    //static hasMany = [entries:Entry
 
     static constraints = {
         usuario(blank:true, unique: true)
         passwd(blank:true, password:true)
+    }
+    
+    String toString() {
+        usuario
     }
 }
