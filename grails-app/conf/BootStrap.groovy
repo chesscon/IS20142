c9d46186723d45a39747982d46f1b4ceec60f6b0
Oscar Hdz C <chesscon@gmail.com>
2014-04-03 21:32:49 -0600

    Se declara un usuario para hacer pruebas

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+
+import proyecto1.Usuario
+
 class BootStrap {
 
     def init = { servletContext ->
+        
+        new Usuario(usuario:"user",passwd:"pass").save()
+        
     }
     def destroy = {
     }
