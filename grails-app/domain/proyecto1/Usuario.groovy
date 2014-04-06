0bed6ac4fa98f955bd28c6f53966d05b43a09cd1
Oscar Hernández <chesscon@gmail.com>
2014-04-06 00:27:09 -0600

    Se añade el atributo tipo (para identificar los diferentes tipos de usuario: 1-> Alumno, 2->Profesor, 3->Escuela)

--- grails-app/domain/proyecto1/Usuario.groovy
+++ grails-app/domain/proyecto1/Usuario.groovy
@@ -2,18 +2,31 @@ package proyecto1
 
 class Usuario {
     
-    String usuario;
-    String passwd;
-    
-    
-    //static hasMany = [entries:Entry
+  String usuario;
+  String passwd;
+  // Tipos: 1 -> Alumno; 2-> Profesor; 3 -> Escuela
+  int tipo = 1;
 
-    static constraints = {
-        usuario(blank:true, unique: true)
-        passwd(blank:true, password:true)
-    }
-    
-    String toString() {
-        usuario
-    }
+  static constraints = {
+      usuario(blank:true, unique: true)
+      passwd(blank:true, password:true)
+      tipo(inList(1, 2, 3))
+  }
+
+  String toString() {
+      usuario
+  }
+  
+  boolean esAlumno() {
+    return tipo == 1;
+  }
+  
+  boolean esProfesor() {
+    return tipo == 2;
+  }
+  
+  boolean esEscuela() {
+    return tipo == 3;
+  }
+  
 }
