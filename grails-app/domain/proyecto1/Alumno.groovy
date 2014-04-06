75838e1d5e5d7c43c658588e13b730917e9fe96f
Oscar Hernández <chesscon@gmail.com>
2014-04-06 00:26:27 -0600

    Se añade el atributo usuario

--- grails-app/domain/proyecto1/Alumno.groovy
+++ grails-app/domain/proyecto1/Alumno.groovy
@@ -1,18 +1,21 @@
 package proyecto1
 
 class Alumno {
-    
-    String nombre;
-    String apPaterno;
-    String apMaterno;
-    String correo;
-    Date fecha_nac;
-    
-    static constraints = {
-        nombre(blank:false)
-        apPaterno(blank:false)
-        apMaterno(blank:false)
-        fecha_nac(blank:false)
-        correo(email:true)
-    }
+  
+  Usuario usuario;
+  String nombre;
+  String apPaterno;
+  String apMaterno;
+  String correo;
+  Date fecha_nac;
+
+  static constraints = {
+      nombre(blank:false)
+      apPaterno(blank:false)
+      apMaterno(blank:false)
+      fecha_nac(blank:false)
+      correo(email:true)
+      usuario(validator: {usuario.isAlumno()})
+  }
+
 }
