84d9281db494058ce8db7cd14207194b17012c06
Oscar Hernández <chesscon@gmail.com>
2014-04-05 23:35:02 -0600

    Se modifica el controlador del usuario para que use el servicio de login

--- grails-app/services/login/LoginService.groovy
+++ grails-app/services/login/LoginService.groovy
@@ -10,9 +10,14 @@ class LoginService {
     def u = Usuario.findByUsuarioAndPasswd(userName, userPass)
     
     if (u) {
+      session.user = user
+      flash.message = "Hello ${user.usuario}!"
+      // regresamos el usuario
       // Aqui se define que hacemos si el usuario existe
+    } else {
+      flash.message = "Sorry, ${params.login}. Please try again."
+      // se devolvera null
     }
-    
   }
 
     def serviceMethod() {
