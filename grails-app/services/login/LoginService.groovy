38afc90f7d343f8656802186ead9e7298be0076c
Oscar Hernández <chesscon@gmail.com>
2014-04-05 22:53:26 -0600

    Servicio de login (se agrega el import que faltaba)

--- grails-app/services/login/LoginService.groovy
+++ grails-app/services/login/LoginService.groovy
@@ -1,9 +1,19 @@
 package login
 
 import grails.transaction.Transactional
+import proyecto1.Usuario
 
 @Transactional
 class LoginService {
+  
+  Usuario doLogin(userName, userPass) {
+    def u = Usuario.findByUsuarioAndPasswd(userName, userPass)
+    
+    if (u) {
+      // Aqui se define que hacemos si el usuario existe
+    }
+    
+  }
 
     def serviceMethod() {
 
