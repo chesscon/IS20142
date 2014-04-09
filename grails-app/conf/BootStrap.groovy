4957f02b39a5c0da2726b85139c9e986c516fde9
Oscar Hernández <chesscon@gmail.com>
2014-04-08 19:41:25 -0500

    Se crear una instancia de un Alumno para realizar algunas pruebas

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,16 @@
 
 import proyecto1.Usuario
+import proyecto1.Alumno
 
 class BootStrap {
 
     def init = { servletContext ->
         
-        new Usuario(usuario:"user",passwd:"pass").save()
+      new Usuario(usuario:"user",passwd:"pass").save()
+      
+      new Alumno(usuario:"user2",passwd:"pass", nombre:"Alumno1", 
+      apPaterno:"Ap Paterno", apMaterno:"Ap Materno", correo: "correo@correo.com",
+      fecha_nac:new Date()).save
         
     }
     def destroy = {
