b0251e54426000a266bb6ac01816fd43c1550c6e
Oscar Hernández <chesscon@gmail.com>
2014-04-15 15:22:47 -0500

    Se añade una instancia de Alumon para pruebas

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,11 +6,11 @@ class BootStrap {
 
     def init = { servletContext ->
         
-      new Usuario(usuario:"user",passwd:"pass").save()
+      //new Usuario(usuario:"user",passwd:"pass").save()
       
-      new Alumno(usuario:"user2",passwd:"pass", nombre:"Alumno1", 
+      new Alumno(usuario:"user",passwd:"pass", nombre:"Alumno1", 
       apPaterno:"Ap Paterno", apMaterno:"Ap Materno", correo: "correo@correo.com",
-      fecha_nac:new Date()).save
+      fecha_nac:new Date()).save()
         
     }
     def destroy = {
