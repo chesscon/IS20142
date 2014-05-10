b8cfd78ea2ec053b4e5370442ed97e499c0236da
Oscar Hernández <chesscon@gmail.com>
2014-05-10 01:37:50 -0500

    OSCAR: Se añade un profesor que se guarda antes de arrancar el servidor para facilitar las profesores

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -13,7 +13,7 @@ class BootStrap {
       apPaterno:"Ap Paterno", apMaterno:"Ap Materno", correo: "correo@correo.com",
       fecha_nac:new Date()).save()
 
-      new Profesor(usuario:"profe",passwd:"pass", nombres:"Profe1", tipo:2, role:"profesor",
+      new Profesor(usuario:"profe",passwd:"pass", nombres:"Profe1", tipo:2, role:"Profesor",
       apellidoPaterno:"Ap Paterno", apellidoMaterno:"Ap Materno", correo: "correo@correo.com",
       fechaDeNacimiento:new Date(), nivel:"Basico", grado_academico:"grado").save()
         
