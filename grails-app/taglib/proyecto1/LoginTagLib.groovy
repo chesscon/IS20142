83aa76ad27e995897715ea7c772830538999d890
Oscar Hernández <chesscon@gmail.com>
2014-05-18 17:13:19 -0500

    OSCAR: se modifica la salida para cuando usuario esta logueado

--- grails-app/taglib/proyecto1/LoginTagLib.groovy
+++ grails-app/taglib/proyecto1/LoginTagLib.groovy
@@ -3,8 +3,9 @@ package proyecto1
 class LoginTagLib {
   def loginControl = {
     if(session.user){
-      out << "Hello ${session.user.usuario} "
-      out << """[${link(action:"logout", controller:"usuario"){"Logout"}}]"""
+      //out << "Hello ${session.user.usuario} "
+      //out << """[${link(action:"logout", controller:"usuario"){"Logout"}}]"""
+      out << render (template: '/layouts/welcome', model: [usuarioInstance: session.user])
     } else {
       //out << """[${link(action:"login", controller:"usuario"){"Login"}}]"""      
       out << render (template:'/layouts/login')
