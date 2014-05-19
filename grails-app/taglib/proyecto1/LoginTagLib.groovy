30bdab72444f347effedc2de08a7b56b7c414551
Oscar Hernández <chesscon@gmail.com>
2014-05-18 23:09:40 -0500

    OSCAR: Se actualiza la forma en que se muestra el enlace para cerra sesión

--- grails-app/taglib/proyecto1/LoginTagLib.groovy
+++ grails-app/taglib/proyecto1/LoginTagLib.groovy
@@ -3,9 +3,11 @@ package proyecto1
 class LoginTagLib {
   def loginControl = {
     if(session.user){
-      //out << "Hello ${session.user.usuario} "
+      out << "<p class='navbar-text navbar-right'>Bienvenido ${session.user.usuario}</a></p>"
+      out << """<p class='navbar-text navbar-right'>[${link(action:"logout", controller:"usuario"){"Cerrar Sesión"}}]</p>"""
+      
       //out << """[${link(action:"logout", controller:"usuario"){"Logout"}}]"""
-      out << render (template: '/layouts/welcome', model: [usuarioInstance: session.user])
+      //out << render (template: '/layouts/welcome', model: [usuarioInstance: session.user])
     } else {
       //out << """[${link(action:"login", controller:"usuario"){"Login"}}]"""      
       out << render (template:'/layouts/login')
