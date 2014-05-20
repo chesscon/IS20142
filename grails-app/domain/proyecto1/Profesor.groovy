8bb0ff0b9c655232391df22c5be6eace4020e34a
Oscar Hdz C <chesscon@gmail.com>
2014-05-20 16:37:13 -0500

    OSCAR: Se actuaaliza estado por default a "aceptado". [TODO: El estado en "espera" sera asginado cuando el profesor se registre]

--- grails-app/domain/proyecto1/Profesor.groovy
+++ grails-app/domain/proyecto1/Profesor.groovy
@@ -9,15 +9,15 @@ class Profesor extends Usuario {
     String nivel
     //String horario
     String video = 'video.mp4'
-    String grado_academico
+    String grado_academico = 'constancia.pdf'
     //String correo
-    String estado = "espera"
+    String estado = "aceptado" // espera se asgin en el controller, en save
     
     static constraints = {
         nombres(blank:false)
         apellidoMaterno(blank:false)
         apellidoPaterno(blank:false)
-        nivel(inList: ["Basico", "Intermedio", "Avanzado"],blank:false)
+        nivel(inList: ["Basico", "Intermedio", "Avanzado", "Conversacion"],blank:false)
         //horario(inList: ["Matutino", "Vespertino", "Mixto"],blank:false )
         video(blank:false)
         grado_academico(blank:false)
